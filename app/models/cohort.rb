9fe14adba39c830a5ea31acc48c6d5947a53e024
QuenMBar <Qbeast360@gmail.com>
2021-04-14 10:33:10 -0700

    commit tree 7b063ea42090e8dc32e89346d24d0582e3f454ff

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -1,3 +1,23 @@
 class Cohort < ActiveRecord::Base
-  
+    has_many :students, class_name: 'Student'
+
+    def add_student(name, age)
+        Student.create(name: name, age: age, cohort: self)
+    end
+
+    def average_age
+        students.average(:age)
+    end
+
+    def total_students
+        students.size
+    end
+
+    def self.biggest
+        all.max_by { |m| m.students.size }
+    end
+
+    def self.sort_by_mod
+        all.sort_by(&:current_mod)
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,29 @@
-puts "Clearing old data..."
+puts 'Clearing old data...'
 Cohort.destroy_all
 Student.destroy_all
 
 # This will create 5 cohorts using a random date
-puts "Seeding cohorts..."
-c1 = Cohort.create(
-  name: "nyc-dumbo-web-#{Faker::Date.forward(years: 1).strftime("%m%d%Y")}",
-  current_mod: rand(1..5)
-)
-c2 = Cohort.create(
-  name: "nyc-dumbo-web-#{Faker::Date.forward(years: 1).strftime("%m%d%Y")}",
-  current_mod: rand(1..5)
-)
-c3 = Cohort.create(
-  name: "nyc-dumbo-web-#{Faker::Date.forward(years: 1).strftime("%m%d%Y")}",
-  current_mod: rand(1..5)
-)
-c4 = Cohort.create(
-  name: "nyc-dumbo-web-#{Faker::Date.forward(years: 1).strftime("%m%d%Y")}",
-  current_mod: rand(1..5)
-)
-c5 = Cohort.create(
-  name: "nyc-dumbo-web-#{Faker::Date.forward(years: 1).strftime("%m%d%Y")}",
-  current_mod: rand(1..5)
-)
+cohorts = []
+puts 'Seeding cohorts...'
+5.times do
+    cohorts <<
+        Cohort.create(
+            name: "nyc-dumbo-web-#{Faker::Date.forward(days: 365).strftime('%m%d%Y')}",
+            current_mod: rand(1..5),
+        )
+end
+# cohorts <<
+#     Cohort.create(name: "nyc-dumbo-web-#{Faker::Date.forward(years: 1).strftime('%m%d%Y')}", current_mod: rand(1..5))
+# cohorts <<
+#     Cohort.create(name: "nyc-dumbo-web-#{Faker::Date.forward(years: 1).strftime('%m%d%Y')}", current_mod: rand(1..5))
+# cohorts <<
+#     Cohort.create(name: "nyc-dumbo-web-#{Faker::Date.forward(years: 1).strftime('%m%d%Y')}", current_mod: rand(1..5))
+# cohorts <<
+#     Cohort.create(name: "nyc-dumbo-web-#{Faker::Date.forward(years: 1).strftime('%m%d%Y')}", current_mod: rand(1..5))
+# cohorts <<
+#     Cohort.create(name: "nyc-dumbo-web-#{Faker::Date.forward(years: 1).strftime('%m%d%Y')}", current_mod: rand(1..5))
 
-puts "Seeding students..."
-# TODO: create seed data for students
-# check your schema and make sure to create students with all the necessary data
+puts 'Seeding students...'
+50.times { Student.create(name: Faker::Name.name, age: rand(5..75), cohort: cohorts.sample) }
 
-puts "Done!"
+puts 'Done!'
