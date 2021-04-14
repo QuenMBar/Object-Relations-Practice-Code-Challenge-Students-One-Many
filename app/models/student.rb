09281a3a8ae3b2a8a8943b67f36516fd61a6c99d
QuenMBar <Qbeast360@gmail.com>
2021-04-14 10:33:16 -0700

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,7 @@
 class Student < ActiveRecord::Base
-  
+    belongs_to :cohort, class_name: 'Cohort', foreign_key: 'cohort_id'
+
+    def current_mod
+        cohort.current_mod
+    end
 end
--- db/migrate/20191229140918_create_cohorts.rb
+++ db/migrate/20191229140918_create_cohorts.rb
@@ -1,8 +1,8 @@
 class CreateCohorts < ActiveRecord::Migration[5.2]
-  def change
-    create_table :cohorts do |t|
-      t.string :name
-      t.integer :current_mod
+    def change
+        create_table :cohorts do |t|
+            t.string :name
+            t.integer :current_mod
+        end
     end
-  end
 end
